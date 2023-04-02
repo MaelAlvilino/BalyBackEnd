f4bd748928aceab95ee8e21c0c1e3472cba69454
MaelAlvilino <emmanuel.alvi@hotmail.com>
2023-04-02 14:39:22 -0300

    change repository

---
+++ users.py
@@ -0,0 +1,12 @@
+from datetime import date
+from dataclasses import dataclass
+@dataclass
+class Usuario:
+    email : str
+    telefone : int
+    cpf  : int
+    nome : str
+    sobrenome : str
+    password : str
+    dt_nasc : date
+    user_type: str
